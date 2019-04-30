7946d4fc5f61277dfd8672aefdd3ae969242bbdf
Kristin Porter <Kristin.Porter@mdrc.org>
2019-04-30 15:22:50 -0700

    comments by Kristin

--- R/metrics.R
+++ R/metrics.R
@@ -1,19 +1,22 @@
 
 # This script contains all the code for ML performance metrics
+KP: Lets rename this script to be more specific about what it contains (metrics could be anything). Maybe predictive performance metrics?
 
-#' Calculates Confusion Matrix (TP, FP, FN, TN) - KP: Spell these out?
+#' Confusion Matrix
 #'
-#' This function takes the predictions of a model,  (can be either binary 0 or 1, or continous numeric [0,1]) and
-#' calculates the confusion metrics based on the predictions.  Given that predictions need to be binary for the confusion matrix to work
-#' you need to pass in a threshold to cut the predictions off.  If the predictions are already binary, then pass in .5
+#' This function produces a confusion matrix - a table that displays the false positive (FP), false negative (FN), true positive(TP), and true negative (results) by comparing a set of predictions to true values.
+#' The predictions can either be binary or continuous. For continuous predictions, a threshold for translating the results to binary classifications must be supplied. If the predictions are already binary, then pass in .5. (KP: Why not just null?)
 #'
-#' @param predictions list of numerics,  predicted values
-#' @param labels list of numerics, actual values/outcomes
-#' @param threshold numeric, value between 0 - 1 to cut  predictions that are continous within binary 0s and 1s
-#' @return table, table of integers with FP, FN, TP,R
-#' @export
+#' @param predictions list (KP: isn't it a numeric vector?) of numerics,  predicted values
+#' @param labels (KP: let's limit jargon and say call this "outcomes" or "trueOutcomes" list of numerics (KP: again use correct R object), actual values/outcomes (KP: if binary, need to be 0/1 yes?)
+#' @param threshold numeric, value between 0 and 1 to translate continuous predictions to binary classifications
+#' @return table (KP: is this an object name or object classification?), table of integers with FP, FN, TP, TN.
+#' @export # KP I don't think the return is correct. It returns a lot more according to the vignette.
 
-get_confusion_matrix <- function(predictions, labels, threshold){
+
+get_confusion_matrix <- function(predictions, labels, threshold){ # KP: can you just have threshold=NULL as default?
+  #KP Maybe not use "get_" Not sure why that is helpful. Maybe we should think about naming conventions? I often use "get" for functions that are designed to grab some subset of information from an R object. Not that we have to use that convention, but as an example of how we might use names.
+
 
   predictions <- as.vector(predictions)
 
@@ -27,6 +30,7 @@ get_confusion_matrix <- function(predictions, labels, threshold){
   return(CM)
 }
 
+KP: What is the point of these functions when these values have already been computed in the above get_confusion_matrix function?
 
 #' Calculates the False Positive Rate (FPR)
 #'
@@ -134,11 +138,11 @@ get_tnr <- function(predictions, labels, threshold){
 }
 
 
-#' Calculates AUC ROC for continious predictions, and actual predictions
-#'
+#' Calculates area under the curve (AUC) for the receiver-operator cureve (ROC) for continious predictions, and actual predictions
+#' KP: The summary above and below don't match.
 #' This function takes the predictions of a model, (can be either binary 0 or 1, or continous numeric [0,1]) and
-#' calculates the roc curve, and then gets the auc under the curve given the predicted values and actual values
-#' Notes : baseline is .5, and perfect is 1
+#' calculates the ROC curve, and then computes the AUC given the predicted values and actual values
+#'
 #'
 #' @param predictions list of numerics,  predicted values
 #' @param labels list of numerics, actual values/outcomes
@@ -263,9 +267,11 @@ get_accuracy <- function(predictions, labels, threshold){
 #' and returns the threshold value that maximizes the specified metric
 #'
 #' @param func function, function that takes arguments, predictions, labels, and threshold
+# #KP Tell the user the options here. But if we discard those functions, we'll need to revise this function.
+
 #' @param predictions list of numerics,  predicted values
 #' @param labels list of numerics, actual values/outcomes
-#' @return numeric, value from 0-1 that is the threshold that maximizes the metric func
+#' @return numeric, value from 0 to 1 that is the threshold that maximizes the metric func
 #' @export
 
 threshold_finder <- function(func, predictions,labels){
