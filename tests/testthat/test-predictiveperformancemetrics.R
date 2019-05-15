9b430f055bbb9801fec6a1299d15a7d858a7ee3d
ksboxer <ksbox12@gmail.com>
2019-05-15 14:15:54 -0400

    first time added

--- tests/testthat/test-predictiveperformancemetrics.R
+++ tests/testthat/test-predictiveperformancemetrics.R
@@ -2,5 +2,5 @@ context("Testing Predictive Performance Metrics Functions")
 library(CDIPATools)
 
 test_that("Confusion Metric works properly",{
-  .......
+  expect_equal(confusion_matrix(c(1,0,1), c(0, 0, 1), .5), caret::confusionMatrix(as.factor(c(1,0,1)), as.factor(c(0, 0, 1)), mode = "everything"))
 })
